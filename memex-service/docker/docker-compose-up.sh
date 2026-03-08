58295c779695d1b6b0b73f6bfdd166dcfcf8e6c6
Matthew Johnson <19312564+matthewjohnson-50788q803@users.noreply.github.com>
2026-03-08 00:01:48 -0600

    Update environment variable prompts in docker script

--- memex-service/docker/docker-compose-up.sh
+++ memex-service/docker/docker-compose-up.sh
@@ -3,22 +3,22 @@
 # script for building docker images and starting them using docker-compose
 # assumes directory of invocation is memex-service/docker, parent of this script
 
-if [[ -z ${MONGO_DEFAULT_USER_USERNAME} ]]; then
-  echo "Enter value for MONGO_DEFAULT_USER_USERNAME: "
-  read MONGO_DEFAULT_USER_USERNAME
-  export MONGO_DEFAULT_USER_USERNAME=${MONGO_DEFAULT_USER_USERNAME}
+if [[ -z ${DEFAULT_USER_USERNAME} ]]; then
+  echo "Enter value for DEFAULT_USER_USERNAME: "
+  read DEFAULT_USER_USERNAME
+  export DEFAULT_USER_USERNAME=${DEFAULT_USER_USERNAME}
 fi
 
-if [[ -z ${MONGO_DEFAULT_USER_PW} ]]; then
-  echo "Enter value for MONGO_DEFAULT_USER_PW: "
-  read MONGO_DEFAULT_USER_PW
-  export MONGO_DEFAULT_USER_PW=${MONGO_DEFAULT_USER_PW}
+if [[ -z ${DEFAULT_USER_PW} ]]; then
+  echo "Enter value for DEFAULT_USER_PW: "
+  read DEFAULT_USER_PW
+  export DEFAULT_USER_PW=${DEFAULT_USER_PW}
 fi
 
-if [[ -z ${TOKEN_ENC_KEY_SECRET} ]]; then
-  echo "Enter value for TOKEN_ENC_KEY_SECRET: "
-  read TOKEN_ENC_KEY_SECRET
-  export TOKEN_ENC_KEY_SECRET=${TOKEN_ENC_KEY_SECRET}
+if [[ -z ${AUTH_TOKEN_ENC_KEY_SECRET} ]]; then
+  echo "Enter value for AUTH_TOKEN_ENC_KEY_SECRET: "
+  read AUTH_TOKEN_ENC_KEY_SECRET
+  export AUTH_TOKEN_ENC_KEY_SECRET=${AUTH_TOKEN_ENC_KEY_SECRET}
 fi
 
 if [[ -z ${USERPASS_ENC_KEY_SECRET} ]]; then
@@ -48,13 +48,13 @@ echo "waiting for app to initialize" && sleep 15
 
 if [[ "${SPRING_PROFILES_ACTIVE}" =~ "dev" ]]; then
   echo "Initializing Mongo userDetails database with encrypted credential"
-  MONGO_DEFAULT_USER_PW_ENCODED=$(curl -X "POST" -H "Content-Type: application/json" -d "{\"password\":\"${MONGO_DEFAULT_USER_PW}\"}" localhost:8080/api/v0/auth/getEncryptedPassword | sed 's/.*{.*"encryptedPassword".*:[^"]*"//' | sed 's/".*}.*//')
+  DEFAULT_USER_PW_ENCODED=$(curl -X "POST" -H "Content-Type: application/json" -d "{\"password\":\"${DEFAULT_USER_PW}\"}" localhost:8080/api/v0/auth/getEncryptedPassword | sed 's/.*{.*"encryptedPassword".*:[^"]*"//' | sed 's/".*}.*//')
   if [[ $? -ne 0 ]]; then
     echo "Mogno userDetails collection was not successfully initialized"
     echo "Failed to retrieve hex encoded encrypted password from memex-service"
     exit 1
   fi
-  cat mongo/dbInit.js | sed "s/\${MONGO_DEFAULT_USER_USERNAME}/${MONGO_DEFAULT_USER_USERNAME}/g" | sed "s/\${MONGO_DEFAULT_USER_PW_ENCODED}/${MONGO_DEFAULT_USER_PW_ENCODED}/g" > mongo/dbInitInterpolated.js
+  cat mongo/dbInit.js | sed "s/\${DEFAULT_USER_USERNAME}/${DEFAULT_USER_USERNAME}/g" | sed "s/\${DEFAULT_USER_PW_ENCODED}/${DEFAULT_USER_PW_ENCODED}/g" > mongo/dbInitInterpolated.js
   mongo < mongo/dbInitInterpolated.js
   if [[ $? -eq 0 ]]; then
     echo "Mongo userDetails collection initialized successfully"
